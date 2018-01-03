9e3fa465228fff25c0a28ecd5b20ca0b8c7051e6
Volkov Alexander <volkov.525@gmail.com>
2018-01-03 16:26:25 +0300

    finished home page with editing

--- src/assets/vis/editor.js
+++ src/assets/vis/editor.js
@@ -1,21 +1,56 @@
 let editMarsh = false;
+let editEt = false;
 
-function closeMarsh() {
+function editMarshrut() {
 	if (!editMarsh) {
 		for (var i = 0; i < 4 ; i++) {
 			$('#marsh').foundation('up', $('#marsh' + i));
 		}
-		$('#marsh ~ .accordion-title').attr('aria-expanded', false)
-		$('#marsh').attr('disabled', '')
-		$('#marshBlock').addClass('edit')
-		$('#marshGear').addClass('edit')
+		$('#marsh > .accordion-title').attr('aria-expanded', false);
+		$('#marsh > li > a').attr('contentEditable', 'true');
+		$('#marsh img.edit').removeClass('hide');
+		$('#marsh').attr('disabled', '');
+		$('#marsh .accordion-title').addClass('disabl');
+		$('#marshBlock').addClass('edit');
+		$('#marshGear').addClass('edit');
+		$('#addMarsh').addClass('hide');
+		$('#addFold').removeClass('hide');
 		editMarsh = true;
 	} else {
 		$('#marsh').removeAttr('disabled')
+		$('#marsh > li > a').attr('contentEditable', 'false');
+		$('#marsh img.edit').addClass('hide')
 		$('#marshBlock').removeClass('edit')
 		$('#marshGear').removeClass('edit')
+		$('#addMarsh').removeClass('hide');
+		$('#addFold').addClass('hide');
 		editMarsh = false;
 	}
-
 }
 
+function editEtap() {
+	if (!editEt) {
+		for (var i = 1; i < 7 ; i++) {
+			$('#etap').foundation('up', $('#etap' + i));
+		}
+		$('#etap > .accordion-title').attr('aria-expanded', false);
+		$('#etap > li > a').attr('contentEditable', 'true');
+		$('#etap img.edit').removeClass('hide');
+		$('#etap').attr('disabled', '');
+		$('#etap .accordion-title').addClass('disabl');
+		$('#etapBlock').addClass('edit');
+		$('#etapGear').addClass('edit');
+		$('#addEt').addClass('hide');
+		$('#addFol').removeClass('hide');
+		editEt = true;
+	} else {
+		$('#etap').removeAttr('disabled')
+		$('#etap > li > a').attr('contentEditable', 'false');
+		$('#etap img.edit').addClass('hide')
+		$('#etapBlock').removeClass('edit')
+		$('#etapGear').removeClass('edit')
+		$('#addEt').removeClass('hide');
+		$('#addFol').addClass('hide');
+		editEt = false;
+	}
+}
