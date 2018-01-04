6906dcbc88a3c63d45e46c52b5c4cb450f51da21
Volkov Alexander <volkov.525@gmail.com>
2018-01-04 13:29:21 +0300

    make draggable divs

--- src/assets/vis/editor.js
+++ src/assets/vis/editor.js
@@ -54,3 +54,60 @@ function editEtap() {
 		editEt = false;
 	}
 }
+function openT() {
+	$('.abs').toggleClass('test')
+}
+
+// ========================================
+// resize panels
+// ========================================
+const $paneContainer = $("#paneContainer");
+const $dragbar = $("#paneContainer #dragbar");
+const $leftPane = $("#paneContainer #editor");
+const $rightPane = $("#paneContainer #viewer");
+
+let dragging = false;
+
+// Todo: Load these from previous session.
+let leftPanePercentage = 50;
+let rightPanePercentage = 50;
+
+function setInitialPaneSizes() {
+  if ($leftPane && $rightPane) {
+    const unit = $paneContainer.width() / (leftPanePercentage + rightPanePercentage);
+    $leftPane.width(unit * leftPanePercentage);
+    $rightPane.width(unit * rightPanePercentage);
+  }
+}
+
+$(document).on("ready", setInitialPaneSizes);
+$(window).on("resize", setInitialPaneSizes);
+
+if ($dragbar) {
+  $dragbar.on("mousedown", (mousedownEvent) => {
+    mousedownEvent.preventDefault();
+    dragging = true;
+    let prevPageX = mousedownEvent.pageX;
+
+    $(document).on("mousemove", (mousemoveEvent) => {
+      if (dragging) {
+        if ($leftPane && $rightPane) {
+          const deltaPageX = mousemoveEvent.pageX - prevPageX;
+          prevPageX = mousemoveEvent.pageX;
+          $leftPane.width($leftPane.width() + deltaPageX);
+          $rightPane.width($rightPane.width() - deltaPageX);
+          leftPanePercentage = 100 / $paneContainer.width() * $leftPane.width();
+          rightPanePercentage = 100 / $paneContainer.width() * $rightPane.width();
+        }
+      }
+    });
+
+    $(document).on("mouseup", () => {
+      if (dragging) {
+        dragging = false;
+        $(document).unbind("mousemove");
+      }
+    });
+  });
+}
+
